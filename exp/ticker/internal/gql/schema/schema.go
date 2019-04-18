b1114440efcef50835cc26b98fd5f8e2bd7cc47d
Alexandre Cordeiro <accordeiro@gmail.com>
2019-04-18 18:59:16 -0300

    exp/ticker: add initial GraphQL schema

---
+++ exp/ticker/internal/gql/schema/schema.go
@@ -0,0 +1,22 @@
+package schema
+
+import "bytes"
+
+//go:generate go-bindata -ignore=\.go -pkg=schema -o=bindata.go ./...
+
+// String reads the .gql schema files from the generated _bindata.go file, concatenating the
+// files together into one string.
+func String() string {
+	buf := bytes.Buffer{}
+	for _, name := range AssetNames() {
+		b := MustAsset(name)
+		buf.Write(b)
+
+		// Add a newline if the file does not end in a newline.
+		if len(b) > 0 && b[len(b)-1] != '\n' {
+			buf.WriteByte('\n')
+		}
+	}
+
+	return buf.String()
+}
