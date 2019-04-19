ff314247460dfdb748294875eeb4b6f842a13e91
Alexandre Cordeiro <accordeiro@gmail.com>
2019-04-19 14:50:54 -0300

    exp/ticker: create initial structure for resolvers / graphql server

---
+++ exp/ticker/internal/gql/main.go
@@ -0,0 +1,55 @@
+package gql
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/stellar/go/exp/ticker/internal/gql/schema"
+	"github.com/stellar/go/exp/ticker/internal/tickerdb"
+)
+
+type Asset tickerdb.Asset
+
+// PartialMarket represents the aggregated market data for a
+// specific pair of assets since <Since>
+type PartialMarket struct {
+	BaseAssetID    int32
+	CounterAssetID int32
+	BaseVolume     float64
+	CounterVolume  float64
+	TradeCount     int32
+	Open           float64
+	Low            float64
+	High           float64
+	Change         float64
+	Close          float64
+	CloseTime      graphql.Time
+	Since          graphql.Time
+}
+
+// PartialAggregatedMarket represents the aggregated market data for
+// a generic trade pair since <Since>
+type PartialAggregatedMarket struct {
+	TradePair     string
+	BaseVolume    float64
+	CounterVolume float64
+	TradeCount    int32
+	Open          float64
+	Low           float64
+	High          float64
+	Change        float64
+	Close         float64
+	CloseTime     graphql.Time
+	Since         graphql.Time
+}
+
+type resolver struct{}
+
+func Serve() {
+	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
+	s := graphql.MustParseSchema(schema.String(), &resolver{}, opts...)
+	http.Handle("/query", &relay.Handler{Schema: s})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ exp/ticker/internal/gql/resolvers.go
@@ -0,0 +1,26 @@
+package gql
+
+func (_ *resolver) Assets() []*Asset {
+	return nil
+}
+
+func (_ *resolver) Markets(
+	args struct {
+		BaseAssetCode      *string
+		BaseAssetIssuer    *string
+		CounterAssetCode   *string
+		CounterAssetIssuer *string
+		NumHoursAgo        *int32
+	},
+) []*PartialMarket {
+	return nil
+}
+
+func (_ *resolver) Ticker(
+	args struct {
+		PairName    *string
+		NumHoursAgo *int32
+	},
+) []*PartialAggregatedMarket {
+	return nil
+}
